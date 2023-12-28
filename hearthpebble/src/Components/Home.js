d4e75ad4d34fd285d61760553215f7ea7c3c001f
Daris Chen <daris.chen@gmail.com>
2023-12-27 16:57:40 -0800

    home page structure

--- hearthpebble/src/Components/Home.js
+++ hearthpebble/src/Components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Home.css';
+import { Link } from 'react-router-dom';
 const images = require.context('./HomeAssets', true);
 const imageList = images.keys().map(image => images(image));
 
@@ -15,6 +16,16 @@ const Button = ({ imageSrc, onClick, isActive }) => {
     );
   };
 
+  const editDeck = () => {
+    return (
+      <Link to="/profile">
+        <button>
+          Edit Deck
+        </button>
+      </Link>
+    );
+  };
+
   const VerticalButtonList = ({ onButtonClick, activeButtonIndex }) => {
     const buttons = [
         { label: 'Button 1', imageSrc: imageList[0] },
@@ -27,6 +38,7 @@ const Button = ({ imageSrc, onClick, isActive }) => {
         { label: 'Button 8', imageSrc: imageList[7] },];
   
     return (
+      <div style={{display: 'flex'}}>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
         {buttons.map((button, index) => (
           <Button
@@ -37,11 +49,60 @@ const Button = ({ imageSrc, onClick, isActive }) => {
           />
         ))}
       </div>
+      </div>
     );
   };
   
+  const Grid = () => {
+    const imageSources = [
+      imageList[0],
+      imageList[1],
+      imageList[2],
+      imageList[3],
+      imageList[4],
+      imageList[5],
+      imageList[6],
+      imageList[7],
+      imageList[0],
+      imageList[1],
+      imageList[2],
+      imageList[3],
+      imageList[4],
+      imageList[5],
+      imageList[6],
+    ];
+
+    return (
+      <div>
+        <Link to="/profile">
+          <button className="Edit Deck" onClick={editDeck} style={{ marginLeft: '140px',
+                                                                    padding: '20px',
+                                                                    placeContent: 'center',
+                                                                    width: '320px' }}>Edit Deck</button>
+        </Link>
+      <div className='Grid' style={{  display: 'flex',
+                    justifyContent: 'flex-end',
+                    marginLeft: '140px' }}>
+
+            <div style={{ display: 'grid',
+                          gridTemplateColumns: 'repeat(3, 1fr)',
+                          gridTemplateRows: 'repeat(5, 100px)',
+                          gap: '10px' }}>
+
+              {imageSources.map((source, index) => (
+                <img key={index}
+                src={source}
+                alt={`Image ${index + 1}`}
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+              ))}
+            </div>
+          </div>
+      </div>
+    );
+  };
+
   const Home = () => {
-    const [activeButtonIndex, setActiveButtonIndex] = useState(null);
+    const [activeButtonIndex, setActiveButtonIndex] = useState(0);
   
     const handleButtonClick = (index) => {
       setActiveButtonIndex(index);
@@ -50,7 +111,13 @@ const Button = ({ imageSrc, onClick, isActive }) => {
     return (
       <div>
         <h1>Home</h1>
-        <VerticalButtonList onButtonClick={handleButtonClick} activeButtonIndex={activeButtonIndex} />
+        <div style={{ display: 'flex' }}>
+          <VerticalButtonList onButtonClick={handleButtonClick} activeButtonIndex={activeButtonIndex}/>
+          <img src={imageList[activeButtonIndex]} style={{ marginLeft: '150px', padding: '50px', width: '800px', placeContent: 'left', height: '800px' }}/>
+          <div style={{ display: 'flex', placeContent: 'right' }}>
+          <Grid/>
+          </div>
+        </div>
       </div>
     );
   };
