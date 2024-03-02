efb9fac902d3c60612a56abac104c00e6d3cd7d0
Parsa Hajipour <47697224+ParsaHaji09@users.noreply.github.com>
2024-03-01 19:19:24 -0800

    temp

--- hearthpebble/src/Components/Pages/Battlefield.js
+++ hearthpebble/src/Components/Pages/Battlefield.js
@@ -1,14 +1,53 @@
 import {io} from 'socket.io-client'
 import './Home.css'
 import { useState, useEffect } from 'react'
+import { CARDS2 } from '../options/cards'
 
 const socket = io('http://localhost:4000')
 
 
-
-
 const Battlefield = () => {
     const [cardName, setCardName] = useState("")
+    const [userCards, setUserCards] = useState([]);
+    
+    function findIndicesByValues(valueList) {
+        const indices = []
+        valueList.forEach(element => {
+          const index = CARDS2.indexOf(element)
+          if(index !== -1){
+            indices.push(index)
+          }
+        });
+        return indices;
+    }
+    
+    const getUser = async (uid) => {
+        try {
+            const response = await axios.get(`http://localhost:5000/api/users/${uid}`);
+            // console.log(response.data); // Handle the response from the server
+            setUserData(response.data);
+            findIndicesByValues(response.data.deck)
+            const correspondingIndexes = findIndicesByValues(response.data.deck)
+            const correspondingCards = correspondingIndexes.map(index => imagesCardsList[index])
+            setUserCards(correspondingCards)
+            setAllCards(allCards.filter(card => !correspondingCards.includes(card)))
+        } catch (error) {
+          console.error('Error getting user data:', error);
+        }
+        finally {
+          setLoading(false); // Set loading to false once data is fetched or if an error occurs
+        }
+    };
+    
+    useEffect(() => {
+        const prevData = localStorage.getItem("saveData");
+        if (!prevData) {
+          navigate('/login');
+        } else {
+          const parsedData = JSON.parse(prevData);
+          getUser(parsedData._id);
+        }
+    }, [navigate]);
 
     const handlePlayCard = (updatedCard) => {
         console.log("pressed");
@@ -19,6 +58,8 @@ const Battlefield = () => {
             setCardName(cardname)
         })
     };
+
+
 
     return (
         <div className='battlefield-body'>
