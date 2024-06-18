dfa3e582c1f702e2e04a577271f113f04158bf1f
Parsa Hajipour <47697224+ParsaHaji09@users.noreply.github.com>
2024-06-17 18:00:09 -0700

    icons on dashboard

--- hearthpebble/src/Components/Navbar.js
+++ hearthpebble/src/Components/Navbar.js
@@ -1,6 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import { logout } from './actions/reduxActions';
+import HomeIcon from '@mui/icons-material/Home';
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import StyleIcon from '@mui/icons-material/Style';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 function Navbar() {
     const dispatch = useDispatch()
@@ -9,16 +13,24 @@ function Navbar() {
         dispatch(logout());
         navigate('/landing')
     }
+
+    const buttonStyle = {
+        all: 'unset',       // Resets all default styles
+        display: 'inline-flex',  // Aligns items and centers them if needed
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        //padding: '8px',
+    };
     return (
         <nav className="nav">
             <a href="/home" className="sitename">
                 <span>Hearthpebble</span>
             </a>
             <div className="links">
-                <a href="/home" className="home">Home</a>
-                <a href="/battlelog" className="battlelog">Battle Log</a>
-                <a href="/editdeck" className="editdeck">Edit Deck</a>
-                <button onClick={handleClick}>Logout</button>
+                <a href="/home" className="home"><HomeIcon/></a>
+                <a href="/battlelog" className="battlelog"><ReceiptLongIcon/></a>
+                <a href="/editdeck" className="editdeck"><StyleIcon/></a>
+                <button className="logout" onClick={handleClick} style = {buttonStyle}><LogoutIcon/></button>
             </div>
         </nav>
     );
