e37f4c90ffa9aef0b64e97e38c16fbf49d639962
Parsa Hajipour <47697224+ParsaHaji09@users.noreply.github.com>
2024-06-22 22:02:18 -0700

    remove register page, now pop up

--- hearthpebble/src/index.js
+++ hearthpebble/src/index.js
@@ -9,11 +9,11 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-    <BrowserRouter>
-        <Routes>
-            <Route path="/*" element = {<App/>} />
-        </Routes>
-    </BrowserRouter>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/*" element = {<App/>} />
+            </Routes>
+        </BrowserRouter>
     </Provider>
 );
 
